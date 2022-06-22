764ca159f8f4ae5d8be653acbe345d01260a31be
foxca <foxca@LAPTOP-UTQBBQF9>
2022-06-22 15:28:55 -0300

    Adicionar arquivos de projeto.

---
+++ atividade1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string texto; // Declaração de variavel.
+            //texto = "Olá mundo ";  // comando que atribue um valor na variavel.
+
+            //Console.Write("Digite o seu nome: "); // comando de saida, mostra o texto "Digite o seu nome: ".
+            //texto = texto + Console.ReadLine(); // variavel texto armazena o valor existente e junta o valor digitado
+            //                                    //recebido pelo comando de entrada (Console.ReadLine()).
+
+            Console.WriteLine("Digite um numero: "); // Comando de saida mostra "Digite um numero:"
+            texto = Console.ReadLine(); // Valor Digitado recebido pelo de entrada e atribuido a variavel texto
+            Console.WriteLine("Digite ouitro numero: "); // Comando de saida mostra "Digite outro numero:"
+            texto = texto + Console.ReadLine(); // variavel texto armazena um valor existente e junta o valor digitado
+
+            Console.Write(texto); //escreve na tela o valor do textoLength, comando Console.WriteLine é de saida.
+
+            Console.ReadKey(); // comando de entrada, espera uma tecla ser clicada para fechar.
+        }
+    }
+}
